33edddc2fc8e4b5c34e216e2f948996c2ab7b27a
Mangasar <mangasarpetrosyan@gmail.com>
2022-09-29 16:28:15 +0300

    add

---
+++ script.js
@@ -0,0 +1,6 @@
+function whyName () {
+    let name = prompt("What is your name?")
+    alert(`Hello ${name} !`); 
+    let age = prompt("How old are you ?")
+    alert(`Your name is ${name}, your ${age} year! `)
+    }
