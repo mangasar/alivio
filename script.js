1ebd407ca03a685db5e659a89f64d0d875c339b2
Mangasar <mangasarpetrosyan@gmail.com>
2022-10-04 11:48:36 +0300

    add

--- script.js
+++ script.js
@@ -3,11 +3,20 @@ function whyName () {
     console.log("Hello" + name);
     var age = prompt("Age?");
     console.log(name + " is " + age + " years old");
-    }
+
+
 
    var content = document.querySelector('.title-hero')
 
-  console.log(content.textContent)
-
+  console.log(content.textContent )
+}
   
+//   const defs = {
+//     prLis : document.querySelector('list-button')
+//   }
 
+// defs.prLis.addEventListener('change' , whyName)
+
+// function changeOnClick () {
+// console.log("change")
+// }
